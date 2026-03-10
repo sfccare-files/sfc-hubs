9dc21acd5c491343271997518b19a3ad79da9224
BM Arafat <alarafat.steadfast@gmail.com>
2026-03-10 01:14:32 +0000

    complete hub locator system

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 var map = L.map('map').setView([23.6850, 90.3563], 7);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
- attribution: '© OpenStreetMap'
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+ attribution:'© OpenStreetMap'
 }).addTo(map);
 
-var marker = L.marker([23.7806, 90.4070]).addTo(map);
+var markers = L.markerClusterGroup();
 
-marker.bindPopup(`
-<b>Dhaka Central Hub</b><br>
-Address: Gulshan 1<br>
-Hub Phone: 017xxxxxx<br>
-Manager: Rahim Ahmed<br>
-<a href="https://www.google.com/maps/dir/?api=1&destination=23.7806,90.4070" target="_blank">Get Directions</a>
-`);
+var hubMarkers = [];
+
+fetch("hubs.json")
+.then(response => response.json())
+.then(data => {
+
+data.forEach(hub => {
+
+var marker = L.marker([hub.lat,hub.lng]);
+
+var popupContent = `
+<b>${hub.name}</b><br>
+Address: ${hub.address}<br>
+Hub Phone: ${hub.hub_phone}<br>
+Manager: ${hub.manager}<br>
+Manager Phone: ${hub.manager_phone}<br><br>
+
+<a target="_blank"
+href="https://www.google.com/maps/dir/?api=1&destination=${hub.lat},${hub.lng}">
+Get Directions
+</a>
+`;
+
+marker.bindPopup(popupContent);
+
+marker.hubName = hub.name.toLowerCase();
+
+markers.addLayer(marker);
+
+hubMarkers.push(marker);
+
+});
+
+map.addLayer(markers);
+
+});
+
+document.getElementById("searchBox").addEventListener("keyup",function(){
+
+var value = this.value.toLowerCase();
+
+hubMarkers.forEach(marker=>{
+
+if(marker.hubName.includes(value)){
+
+map.setView(marker.getLatLng(),12);
+
+marker.openPopup();
+
+}
+
+});
+
+});
